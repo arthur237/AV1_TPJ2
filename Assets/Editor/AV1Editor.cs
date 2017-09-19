8913ef169b5bc92b9c9380e5af049b61b9f1fac9
Gabriel Santos <gss2680@gmail.com>
2017-09-19 14:33:03 -0300

    hfgl

---
+++ Assets/Editor/AV1Editor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AV1Behaviour))]
+public class AV1Editor : Editor
+{
+    int value;
+    int index;
+   
+    public override void OnInspectorGUI()
+    {
+        AV1Behaviour AV1Behaviour = target as AV1Behaviour;
+        
+        
+        value = EditorGUILayout.IntField("Valor", value);
+
+
+        if (GUILayout.Button("Adicionar"))
+        {
+            AV1Behaviour.Add(AV1Behaviour.List.Count.ToString(), value);
+        }
+        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.LabelField("Remover", EditorStyles.boldLabel);
+        index = EditorGUILayout.IntField("Valor", index);
+        if (GUILayout.Button("Remover"))
+        {
+            AV1Behaviour.Remove(index);
+        }
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.LabelField("Lista", EditorStyles.boldLabel);
+        
+        for (int i = 0; i < AV1Behaviour.List.Count; i++)
+        {
+            EditorGUILayout.FloatField(AV1Behaviour.List[i].key, AV1Behaviour.List[i].value);
+        }  
+        EditorGUILayout.LabelField("Adicionar", EditorStyles.boldLabel);
+       
+       
+        
+      
+
+        
+    }
+
+}
